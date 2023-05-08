8e21d87ce09c17d312344af1b8eb87196988bfda
castellano17 <castellano17@gmail.com>
2023-05-07 20:10:55 -0600

    add states in the locaStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
-  const [patients, setPatients] = useState([]);
+  const [patients, setPatients] = useState(
+    JSON.parse(localStorage.getItem("patients")) ?? []
+  );
   const [patient, setPatient] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("patients", JSON.stringify(patients));
+  }, [patients]);
 
   const deletePatient = (id) => {
     const patientsUpdates = patients.filter((patient) => patient.id !== id);
